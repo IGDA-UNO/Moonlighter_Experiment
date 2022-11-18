45a69db6f0e4cb1f07cb81e27c280bcee98b10ef
Hardcore5412 <jzhang17@uno.edu>
2022-11-18 17:18:25 -0600

    Pathfinding AI, New Monster Type.

---
+++ Assets/Scripts/Spit_Projectile.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spit_Projectile : MonoBehaviour
+{
+    public float moveSpeed;
+    private Transform attackTarget;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        attackTarget = GameObject.FindGameObjectWithTag("Player").transform;
+       Invoke("selfDestruct", 4);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.TransformDirection(Vector3.right) * moveSpeed * Time.deltaTime;
+
+    }
+    private void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.tag == "Player" ){
+            Debug.Log("Player hit");
+            Destroy(this.gameObject);
+        }
+    }
+    private void selfDestruct(){
+        Destroy(this.gameObject);
+    }
+
+   
+}
