090da06da51fc2a37d39b902d08c9314ab17b38f
raautin <raautin@uno.edu>
2023-06-23 17:34:39 -0500

    IDGA end of semester demo

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,15 +9,17 @@ using UnityEngine.Events;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] GameObject MainMenuUI;
 
      public UnityEvent StartButton;
 
     public void PlayGame()
     {
         StartButton.Invoke();
-        SceneManager.LoadScene("MAPscene");
-        SceneManager.LoadScene("Lenaoria & Christinas scene", LoadSceneMode.Additive);
-      
+        SceneManager.LoadScene("Lenaoria & Christinas scene");
+        SceneManager.LoadScene("CityScene", LoadSceneMode.Additive);
+      MainMenuUI.SetActive(false);
+
         //, LoadSceneMode.Additive
     }
 
