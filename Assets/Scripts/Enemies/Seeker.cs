83f9eb069f04e56c7c966e8d9792cb6f895e2b5d
KazztheCajun <aautincreations@gmail.com>
2024-02-09 18:31:22 -0600

    Seeker Enemy Logic Updated

--- Assets/Scripts/Enemies/Seeker.cs
+++ Assets/Scripts/Enemies/Seeker.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class Seeker : Enemy
 {
+    [Range(0f, 20f)]
+    public float rotationOffset;
+    public Transform debugPoint;
+
+    Vector3 point;
+
+    void Start()
+    {
+        base.Start();
+        ChangeState(EnemyState.PATROL);
+        point = debugPoint.position;
+    }
+
+
     public override void Idle()
     {
 
@@ -11,12 +25,33 @@ public class Seeker : Enemy
 
     public override void Patrol()
     {
-
+        WalkAround();
     }
 
     public override void Attack()
     {
-        
+        //transform.LookAt(target, Vector3.down);
+        LookAtPlayer();
+    }
+
+    public void WalkAround()
+    {
+        Debug.DrawRay(transform.position, point - transform.position, Color.cyan);
+        if(Vector3.Distance(transform.position, point) <= rotationOffset)
+        {
+            transform.RotateAround(point, Vector3.forward, walkSpeed * 10 * Time.deltaTime);
+        }
+        else
+        {
+            Move(point, "walk");
+        }
+    }
+
+    public void LookAtPlayer()
+    {
+        Vector3 dir = target.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 
     public override void DamagePlayer()
